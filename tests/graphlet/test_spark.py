f2d8b10809c50cd6c08513871582bd48fe01a1b1
Russell Jurney <rjurney@archipelo.co>
2022-07-31 16:52:24 -0700

    Created a spark_session fixture in tests/graphlet/test_spark.py

--- tests/graphlet/test_spark.py
+++ tests/graphlet/test_spark.py
@@ -1,1 +1,16 @@
 """Implements unit tests of Graphlet's spark module."""
+
+import pytest  # type: ignore
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture
+def spark_session(app_name="PyTest fixture SparkSession") -> SparkSession:
+    """spark_session generate a SparkSession for unit tests.
+
+    Returns
+    -------
+    SparkSession
+        A SparkSession in a local environment
+    """
+    return SparkSession.builder.appName(app_name).getOrCreate()
