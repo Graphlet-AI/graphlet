8054f3e84b9b90c333894bbea95cda61b343934d
Russell Jurney <rjurney@archipelo.co>
2022-08-03 16:03:39 -0700

    Make version test pass

--- tests/test_graphlet.py
+++ tests/test_graphlet.py
@@ -4,4 +4,4 @@ from graphlet import __version__
 
 def test_version():
     """test_version Make sure the package version is accurate."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
