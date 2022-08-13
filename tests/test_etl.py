8d59b981b2bbbdb34803409cab7409e014843c98
Russell Jurney <rjurney@archipelo.co>
2022-08-13 03:46:07 -0700

    Spark ETL test code

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,10 +1,12 @@
 """Implements unit tests of Graphlet's spark module."""
 
 import re
+import typing
 
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
 import pytest
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType
 
 from graphlet.etl import EdgeBase, EntityBase, NodeBase
 
@@ -71,20 +73,21 @@ def test_entity_pyspark() -> None:
     """test_entity_pyspark Verify we can use the class in PySpark."""
 
     entity = TestEntity()
-    assert StructType.fromJson(entity.spark_schema())
+    assert T.StructType.fromJson(entity.spark_schema())
 
 
 class Movie(NodeBase):
     """Movie genres."""
 
     entity_type = "movie"
-    genre: str
+    title: str
+    genre: typing.Optional[str]
 
 
 def test_node() -> None:
     """test_node Test the EdgeBase class."""
 
-    comedy_node = Movie(genre="comedy")
+    comedy_node = Movie(title="Coming to America", genre="comedy")
     assert comedy_node.genre == "comedy"
 
 
@@ -93,6 +96,7 @@ class Director(NodeBase):
 
     entity_type = "director"
     name: str
+    nationality: typing.Optional[str]
 
 
 class Directed(EdgeBase):
@@ -104,12 +108,12 @@ class Directed(EdgeBase):
 def test_nodes_edge() -> None:
     """test_edge Test the Relationship class."""
 
-    comedy_node = Movie(genre="comedy")
+    comedy_node = Movie(title="Trauma", genre="horror")
     director_node = Director(name="Dario Argento")
     director_edge = Directed(src=director_node.entity_id, dst=comedy_node.entity_id, entity_type="director")
 
     # Test ze nodes
-    assert comedy_node.genre == "comedy"
+    assert comedy_node.genre == "horror"
     assert director_node.name == "Dario Argento"
 
     # Test ze edge
@@ -125,7 +129,91 @@ class NoType(EntityBase):
 
 def test_default_entity_type() -> None:
     """Verify that without a entity_type argument, the entity type defaults to the class name."""
+
     default_type_entity = NoType()
     print(f"type(default_type_entity.entity_type) == {type(default_type_entity.entity_type)}")
     print(f"default_type_entity.entity_type == {default_type_entity.entity_type}")
     assert default_type_entity.entity_type == "notype"
+
+
+@F.udf(T.StringType())
+def standard_unrated(x):
+    """standard_unrated Standardize different forms of unrated films.
+
+    Parameters
+    ----------
+    x : str
+        movie rating
+
+    Returns
+    -------
+    str
+        The standard rating.
+    """
+    rating: str = "Unknown"
+    if ("not" in x.lower()) or ("un" in x.lower()):
+        rating = "Unrated"
+
+    if x.upper() in ["G", "PG", "PG-13", "R", "X", "XX", "XXX"]:
+        rating = x.upper()
+
+    return rating
+
+
+@F.udf(T.IntegerType())
+def text_runtime_to_minutes(x):
+    """text_runtime_to_minutes Turn a text runtime to minutes.
+
+    Parameters
+    ----------
+    x : str
+        Raw text movie runtime field: ex. "1h 34m"
+
+    Returns
+    -------
+    int
+        minutes of runtime
+    """
+    hour_min = x.split(" ")
+    hours = int(hour_min[0][:-1])
+    mins = int(hour_min[1][:-1])
+
+    return (60 * hours) + mins
+
+
+def test_spark_etl(spark_session) -> None:
+    """Test the classes with Spark UDFs."""
+
+    # Movie awards
+    awards = spark_session.read.option("header", "true").csv("tests/data/awards.csv")
+    awards.show()
+
+    # A genre of movies
+    comedies = spark_session.read.option("header", "true").csv("tests/data/comedy.csv")
+    comedies.show()
+
+    # Another genre of movies
+    horror = spark_session.read.option("header", "true").csv("tests/data/horror.csv")
+    horror.show()
+
+    comedy_movies = comedies.select(
+        F.lit("movie").alias("entity_type"),
+        F.lit("comady").alias("genre"),
+        "title",
+        "year",
+        "length",
+        "gross",
+        F.lit(None).alias("rating"),
+    )
+    comedy_movies.count()
+
+    horror_movies = horror.select(
+        F.lit("movie").alias("entity_type"),
+        F.lit("horror").alias("genre"),
+        F.col("Title").alias("title"),
+        F.col("Year").alias("year"),
+        text_runtime_to_minutes("Length").alias("length"),
+        F.lit(None).alias("gross"),
+        standard_unrated("Rating").alias("rating"),
+    )
+    horror_movies.count()
