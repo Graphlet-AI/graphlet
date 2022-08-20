85afede74b907c59980bf9d797ce9f716ca97ed8
Russell Jurney <russell.jurney@gmail.com>
2022-08-19 17:52:34 -0700

    Commented out unit test for pydantic spark stuff

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -3,31 +3,45 @@
 import re
 import typing
 
+# import pandas as pd
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 import pytest
+
+# from pydantic import validator
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
 from graphlet.etl import EdgeBase, EntityBase, NodeBase
 
 
 @pytest.fixture
-def spark_session(app_name="PyTest fixture SparkSession") -> SparkSession:
-    """spark_session generate a SparkSession for unit tests.
+def spark_session_context(app_name="PyTest fixture SparkSession") -> typing.Tuple[SparkSession, SparkContext]:
+    """spark_session_context generate a SparkSession its SparkContext for unit tests.
+
+    Parameters
+    ----------
+    app_name : str, optional
+        Spark application name, by default "PyTest fixture SparkSession"
 
     Returns
     -------
-    SparkSession
-        A SparkSession in a local environment
+    typing.Tuple[SparkSession, SparkContext]
+        A SparkSession and SparkContext in a local environment
     """
-    return SparkSession.builder.appName(app_name).getOrCreate()
+
+    spark = SparkSession.builder.appName(app_name).getOrCreate()
+    sc = spark.sparkContext
+    return spark, sc
 
 
-def test_spark_session_fixture(spark_session: SparkSession) -> None:
+def test_spark_session_fixture(spark_session_context: typing.Tuple[SparkSession, SparkContext]) -> None:
     """test_spark_session_fixture Make sure the SparkSession is created."""
 
+    spark, sc = spark_session_context
+
     data = [("a", "b"), ("c", "d")]
-    df = spark_session.createDataFrame(data, ["x", "y"])
+    df = spark.createDataFrame(data, ["x", "y"])
     assert df.count() == 2
     assert df.collect() == [("a", "b"), ("c", "d")]
 
@@ -160,8 +174,7 @@ def standard_unrated(x):
     return rating
 
 
-@F.udf(T.IntegerType())
-def text_runtime_to_minutes(x):
+def text_runtime_to_minutes(x: str) -> int:
     """text_runtime_to_minutes Turn a text runtime to minutes.
 
     Parameters
@@ -181,21 +194,30 @@ def text_runtime_to_minutes(x):
     return (60 * hours) + mins
 
 
-def test_spark_etl(spark_session) -> None:
+@F.udf(T.LongType())
+def text_runtime_to_minutes_old_udf(x: str) -> int:
+    """Normal PySpark UDF to convert text runtime to integer minutes."""
+    return text_runtime_to_minutes(x)
+
+
+def test_traditional_spark_etl(spark_session_context: typing.Tuple[SparkSession, SparkContext]) -> None:
     """Test the classes with Spark UDFs."""
 
+    spark, sc = spark_session_context
+
     # Movie awards
-    awards = spark_session.read.option("header", "true").csv("tests/data/awards.csv")
+    awards = spark.read.option("header", "true").csv("tests/data/awards.csv")
     awards.show()
 
     # A genre of movies
-    comedies = spark_session.read.option("header", "true").csv("tests/data/comedy.csv")
+    comedies = spark.read.option("header", "true").csv("tests/data/comedy.csv")
     comedies.show()
 
     # Another genre of movies
-    horror = spark_session.read.option("header", "true").csv("tests/data/horror.csv")
+    horror = spark.read.option("header", "true").csv("tests/data/horror.csv")
     horror.show()
 
+    # Transform comedies into generic movies
     comedy_movies = comedies.select(
         F.lit("movie").alias("entity_type"),
         F.lit("comady").alias("genre"),
@@ -205,15 +227,122 @@ def test_spark_etl(spark_session) -> None:
         "gross",
         F.lit(None).alias("rating"),
     )
-    comedy_movies.count()
+    comedy_movies.show()
 
+    # Transform horror films into generic movies
     horror_movies = horror.select(
         F.lit("movie").alias("entity_type"),
         F.lit("horror").alias("genre"),
         F.col("Title").alias("title"),
         F.col("Year").alias("year"),
-        text_runtime_to_minutes("Length").alias("length"),
+        text_runtime_to_minutes_old_udf("Length").alias("length"),
         F.lit(None).alias("gross"),
         standard_unrated("Rating").alias("rating"),
     )
-    horror_movies.count()
+    horror_movies.show()
+
+
+# def test_graphlet_etl(spark_session_context) -> None:
+#     """Test the classes with Spark UDFs."""
+
+#     spark, sc = spark_session_context
+
+#     @F.pandas_udf("long")
+#     def text_runtime_to_minutes_pandas_udf(x: pd.Series) -> pd.Series:
+#         """text_runtime_to_minutes_pandas_udf PySpark pandas_udf to run text_runtime_to_minutes.
+
+#         Parameters
+#         ----------
+#         x: pd.Series
+#             Column to run text_runtime_to_minutes on
+
+#         Returns
+#         -------
+#         pd.Series
+#             a Column of integers
+#         """
+#         return x.apply(text_runtime_to_minutes)
+
+#     # Movie awards
+#     awards = spark.read.option("header", "true").csv("tests/data/awards.csv")
+#     awards.show()
+
+#     # A genre of movies
+#     comedies = spark.read.option("header", "true").csv("tests/data/comedy.csv")
+#     comedies.show()
+
+#     # Another genre of movies
+#     horror = spark.read.option("header", "true").csv("tests/data/horror.csv")
+#     horror.show()
+
+#     class Movie(NodeBase):
+#         """A film node in hollywood."""
+
+#         entity_type: str = "movie"
+#         genre: str
+#         title: str
+#         year: str
+#         length: int = 0
+#         gross: int = 0
+#         rating: str
+
+#         @validator("length", pre=True)
+#         def convert_hours_minutes_to_int_minutes(cls, x):
+#             if x and isinstance(x, str):
+#                 x = text_runtime_to_minutes(x)
+#             return x
+
+#     class Person(NodeBase):
+#         """A class about people in the film industry."""
+
+#         entity_type = "person"
+#         name: str
+#         role: str
+
+#     class Directed(EdgeBase):
+#         """A director directed a movie."""
+
+#         entity_type = "directed"
+
+#     class ActedIn(EdgeBase):
+#         """An actor acted in a movie."""
+
+#         entity_type = "acted_in"
+
+#     def horror_to_movie(dfs: typing.Iterable[pd.DataFrame]) -> typing.Iterable[pd.DataFrame]:
+#         """horror_to_movie Iterates entire pandas DataFrames, transforming from original schema to the pydantic schema.
+
+#         Parameters
+#         ----------
+#         x : typing.Iterable[pd.DataFrame]
+#             Accepts an iterable of pd.DataFrames
+
+#         Returns
+#         -------
+#         typing.Iterable[pd.DataFrame]
+
+#         Yields
+#         ------
+#         Iterator[typing.Iterable[pd.DataFrame]]
+#             pd.DataFrames of the new schema
+#         """
+
+#         def map_to_movie(x):
+#             """Map the DataFrame rows to a Movie, validate them, then back out to a dict."""
+#             print(x, type(x))
+#             return Movie(
+#                 genre="horror",
+#                 title=x["Title"],
+#                 year=x["Year"],
+#                 length=x["Length"],
+#                 rating=x["Rating"],
+#             ).dict()
+
+#         for df in dfs:
+#             # yield pd.concat(pd.DataFrame(x) for x in df.map(map_to_movie))
+#             yield df.apply(map_to_movie, axis=1)
+
+#     movie_schema = T.StructType.fromJson(Movie.spark_schema())
+
+#     horror_movies = horror.mapInPandas(horror_to_movie, schema=movie_schema)
+#     horror_movies.show()
