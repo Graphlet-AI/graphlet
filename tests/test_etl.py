0cb5f5e139a6dd8be1bfe04720ae055bb6fd3ed3
Russell Jurney <russell.jurney@gmail.com>
2022-08-25 21:17:18 -0700

    Removed some old stuff

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -44,80 +44,6 @@ def test_spark_session_fixture(spark_session_context: typing.Tuple[SparkSession,
     assert df.collect() == [("a", "b"), ("c", "d")]
 
 
-# def test_entity_id() -> None:
-#     """test_entity_base_id Verify that the EntityBase auto generated entity_id is a valid UUID4."""
-
-#     # Verify that the UUID4 works
-#     entity = TestEntity()
-#     assert re.search(r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", entity.entity_id)
-
-
-# def test_entity_base_enum() -> None:
-#     """test_entity_base Verify that the EntityBase types are working."""
-
-#     # Verify that the type is as expected
-#     entity1 = TestEntity()
-#     assert entity1.entity_id is not None
-#     assert entity1.entity_type == "test_entity"
-
-#     # Verify that the type is as expected
-#     try:
-#         TestEntity(entity_type=None)
-#     except ValueError:
-#         pytest.fail("Set null entity type")
-
-
-# class Movie(NodeBase):
-#     """Movie genres."""
-
-#     entity_type = "movie"
-#     title: str
-#     genre: typing.Optional[str]
-
-
-# def test_node() -> None:
-#     """test_node Test the EdgeBase class."""
-
-#     comedy_node = Movie(title="Coming to America", genre="comedy")
-#     assert comedy_node.genre == "comedy"
-
-
-# class Director(NodeBase):
-#     """A person in hollywood."""
-
-#     entity_type = "director"
-#     name: str
-#     nationality: typing.Optional[str]
-
-
-# class Directed(EdgeBase):
-#     """A director directed a movie."""
-
-#     entity_type = "directed"
-
-
-# def test_nodes_edge() -> None:
-#     """test_edge Test the Relationship class."""
-
-#     comedy_node = Movie(title="Trauma", genre="horror")
-#     director_node = Director(name="Dario Argento")
-#     director_edge = Directed(src=director_node.entity_id, dst=comedy_node.entity_id, entity_type="director")
-
-#     # Test ze nodes
-#     assert comedy_node.genre == "horror"
-#     assert director_node.name == "Dario Argento"
-
-#     # Test ze edge
-#     assert director_edge.src == director_node.entity_id
-#     assert director_edge.dst == comedy_node.entity_id
-
-
-# class NoType(EntityBase):
-#     """An entity without a specified type."""
-
-#     pass
-
-
 def standard_unrated(x):
     """standard_unrated Standardize different forms of unrated films.
 
@@ -271,108 +197,70 @@ def test_pandas_spark_etl(spark_session_context: typing.Tuple[SparkSession, Spar
 # def test_base_classes() -> None:
 #     """Test the graplet.etl base classes for nodes and edges."""
 
+# def test_entity_id() -> None:
+#     """test_entity_base_id Verify that the EntityBase auto generated entity_id is a valid UUID4."""
 
-# def test_graphlet_etl(spark_session_context) -> None:
-#     """Test the classes with Spark UDFs."""
+#     # Verify that the UUID4 works
+#     entity = TestEntity()
+#     assert re.search(r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", entity.entity_id)
 
-#     spark, sc = spark_session_context
 
-#     @F.pandas_udf("long")
-#     def text_runtime_to_minutes_pandas_udf(x: pd.Series) -> pd.Series:
-#         """text_runtime_to_minutes_pandas_udf PySpark pandas_udf to run text_runtime_to_minutes.
+# def test_entity_base_enum() -> None:
+#     """test_entity_base Verify that the EntityBase types are working."""
 
-#         Parameters
-#         ----------
-#         x: pd.Series
-#             Column to run text_runtime_to_minutes on
+#     # Verify that the type is as expected
+#     entity1 = TestEntity()
+#     assert entity1.entity_id is not None
+#     assert entity1.entity_type == "test_entity"
 
-#         Returns
-#         -------
-#         pd.Series
-#             a Column of integers
-#         """
-#         return x.apply(text_runtime_to_minutes)
+#     # Verify that the type is as expected
+#     try:
+#         TestEntity(entity_type=None)
+#     except ValueError:
+#         pytest.fail("Set null entity type")
 
-#     # Movie awards
-#     awards = spark.read.option("header", "true").csv("tests/data/awards.csv")
-#     awards.show()
+# class Movie(NodeBase):
+#     """Movie genres."""
 
-#     # A genre of movies
-#     comedies = spark.read.option("header", "true").csv("tests/data/comedy.csv")
-#     comedies.show()
+#     entity_type = "movie"
+#     title: str
+#     genre: typing.Optional[str]
 
-#     # Another genre of movies
-#     horror = spark.read.option("header", "true").csv("tests/data/horror.csv")
-#     horror.show()
+# def test_node() -> None:
+#     """test_node Test the EdgeBase class."""
 
-#     class Movie(NodeBase):
-#         """A film node in hollywood."""
+#     comedy_node = Movie(title="Coming to America", genre="comedy")
+#     assert comedy_node.genre == "comedy"
 
-#         entity_type: str = "movie"
-#         genre: str
-#         title: str
-#         year: str
-#         length: int = 0
-#         gross: int = 0
-#         rating: str
 
-#         @validator("length", pre=True)
-#         def convert_hours_minutes_to_int_minutes(cls, x):
-#             if x and isinstance(x, str):
-#                 x = text_runtime_to_minutes(x)
-#             return x
+# class Director(NodeBase):
+#     """A person in hollywood."""
 
-#     class Person(NodeBase):
-#         """A class about people in the film industry."""
+#     entity_type = "director"
+#     name: str
+#     nationality: typing.Optional[str]
 
-#         entity_type = "person"
-#         name: str
-#         role: str
+# class Directed(EdgeBase):
+#     """A director directed a movie."""
 
-#     class Directed(EdgeBase):
-#         """A director directed a movie."""
+#     entity_type = "directed"
 
-#         entity_type = "directed"
+# def test_nodes_edge() -> None:
+#     """test_edge Test the Relationship class."""
 
-#     class ActedIn(EdgeBase):
-#         """An actor acted in a movie."""
+#     comedy_node = Movie(title="Trauma", genre="horror")
+#     director_node = Director(name="Dario Argento")
+#     director_edge = Directed(src=director_node.entity_id, dst=comedy_node.entity_id, entity_type="director")
 
-#         entity_type = "acted_in"
+#     # Test ze nodes
+#     assert comedy_node.genre == "horror"
+#     assert director_node.name == "Dario Argento"
 
-#     def horror_to_movie(dfs: typing.Iterable[pd.DataFrame]) -> typing.Iterable[pd.DataFrame]:
-#         """horror_to_movie Iterates entire pandas DataFrames, transforming from original schema to the pydantic schema.
+#     # Test ze edge
+#     assert director_edge.src == director_node.entity_id
+#     assert director_edge.dst == comedy_node.entity_id
 
-#         Parameters
-#         ----------
-#         x : typing.Iterable[pd.DataFrame]
-#             Accepts an iterable of pd.DataFrames
+# class NoType(EntityBase):
+#     """An entity without a specified type."""
 
-#         Returns
-#         -------
-#         typing.Iterable[pd.DataFrame]
-
-#         Yields
-#         ------
-#         Iterator[typing.Iterable[pd.DataFrame]]
-#             pd.DataFrames of the new schema
-#         """
-
-#         def map_to_movie(x):
-#             """Map the DataFrame rows to a Movie, validate them, then back out to a dict."""
-#             print(x, type(x))
-#             return Movie(
-#                 genre="horror",
-#                 title=x["Title"],
-#                 year=x["Year"],
-#                 length=x["Length"],
-#                 rating=x["Rating"],
-#             ).dict()
-
-#         for df in dfs:
-#             # yield pd.concat(pd.DataFrame(x) for x in df.map(map_to_movie))
-#             yield df.apply(map_to_movie, axis=1)
-
-#     movie_schema = T.StructType.fromJson(Movie.spark_schema())
-
-#     horror_movies = horror.mapInPandas(horror_to_movie, schema=movie_schema)
-#     horror_movies.show()
+#     pass
